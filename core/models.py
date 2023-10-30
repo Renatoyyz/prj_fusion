c35996a500b825267b775349828e0fe9f74942d3
Renatoyyz <witcarerenato@gmail.com>
2023-10-30 08:44:34 -0300

    Ajustando templates e models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+class Base(models.Model):
+    criados = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateTimeField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+
+    servico = models.CharField('Serviço', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    icone = models.CharField('Icone', choices=ICONE_CHOICES, max_length=14)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self) -> str:
+        return self.servico
