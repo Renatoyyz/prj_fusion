3534ab96b7fd704d8028f21b8f9de52ce93f6c79
Renatoyyz <witcarerenato@gmail.com>
2023-10-30 21:39:52 -0300

    Criando Modelos

--- core/models.py
+++ core/models.py
@@ -29,3 +29,28 @@ class Servico(Base):
 
     def __str__(self) -> str:
         return self.servico
+    
+class Cargo(Base):
+    cargo = models.CharField('Cargo', max_length=100)
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self) -> str:
+        return self.cargo
+    
+class Funcionario(Base):
+    nome = models.CharField('Nome', max_length=100)
+    cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
+    bio = models.TextField('Bio', max_length=200)
+    imagem = StdImageField('Imagem', upload_to='equipe', variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    facebook = models.CharField('Facebook', max_length=100, default='#')
+    twitter = models.CharField('Twitter', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+        
+    def __str__(self) -> str:
+        return f'{self.nome} - {self.get_cargo}'
